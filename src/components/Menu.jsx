1ebde47f7e44cad756d01bc64c6481d034b62b2b
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-11-25 10:37:59 -0600

    added Menu& MenuData components for nav dropdown

---
+++ src/components/Menu.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { IconContext } from "react-icons";
+import { Link } from "react-router-dom";
+import { MenuData } from "./MenuData";
+import * as TiIcons from "react-icons/ti";
+import * as AiIcons from "react-icons/ai";
+import "./css/Menu.css";
+
+const Menu = () => {
+  const [menu, setMenu] = useState(false);
+  const showMenu = () => setMenu(!menu);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="nav">
+          <Link to="#" className="burger">
+            <TiIcons.TiThMenu onClick={showMenu} />
+          </Link>
+          <div>Bytor</div>
+          <nav className={menu ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-menu-items" onClick={showMenu}>
+              <li className="nav-toggle">
+                <Link to="#" className="closeIcon">
+                  <AiIcons.AiOutlineCloseCircle />
+                </Link>
+              </li>
+
+              {MenuData.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </IconContext.Provider>
+    </>
+  );
+};
+
+export default Menu;
---
+++ src/components/MenuData.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import * as BsIcons from "react-icons/bs"
+import * as MdIcons from "react-icons/md"
+import * as RiIcons from "react-icons/ri"
+import * as GoIcons from "react-icons/go"
+
+export const MenuData = [
+    {
+        title: "About",
+        path: "/",
+        icon: <BsIcons.BsInfoCircle />,
+        cName: "menu-text",
+    },
+    {
+        title: "Contact",
+        path: "/contact",
+        icon: <GoIcons.GoMail />,
+        cName: "menu-text",
+    },
+    {
+        title: "Portfolio",
+        path: "/portfolio",
+        icon: <MdIcons.MdImageSearch/>,
+        cName: "menu-text",
+    },
+    {
+        title: "Resume",
+        path: "/resume",
+        icon: <RiIcons.RiFolderUserLine />,
+        cName: "menu-text",
+    }
+];
