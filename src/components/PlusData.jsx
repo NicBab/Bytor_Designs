9a596aaefa12498b87b9ae38007d861cd5a1c86e
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-11-27 13:36:32 -0600

    subtracted usused followIcons

--- src/components/PlusData.jsx
+++ src/components/PlusData.jsx
@@ -7,12 +7,6 @@ import * as BsIcons from 'react-icons/bs'
 
 export const plusData = [
     {
-        title: "LinkedIn",
-        path: "",
-        icon: <GrIcons.GrLinkedinOption />,
-        cName: "plus-text",
-    },
-    {
         title: "Facebook",
         url: "https://www.facebook.com/bytordesign",
         icon: <ImIcons.ImFacebook />,
@@ -22,12 +16,6 @@ export const plusData = [
         title: "Instagram",
         url: "https://www.instagram.com/xoxbytorxox/?fbclid=IwAR19MgrBbb4N8hvQPD_FDv5I2ozZsneg21j4e9zJ0Y_I1ORWFM1C-_Yz804",
         icon: <BsIcons.BsInstagram/>,
-        cName: "plus-text",
-    },
-    {
-        title: "Twitter",
-        url: "",
-        icon: <BsIcons.BsTwitter/>,
         cName: "plus-text",
     },
     {
