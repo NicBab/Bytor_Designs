cdc5bbcf6fa90a9125b6be09a27cb30467ab2e76
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-11-29 20:48:30 -0600

    logo font size

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-import React from "react";
+import React from 'react'
 import "./css/Portfolio.css";
 import { Container } from "react-bootstrap";
 import birdRainbow from '../Images/BIRDRNBOW.png'
@@ -8,68 +8,57 @@ import bonkerz2 from '../Images/bonkerz2.png'
 import firewerks from '../Images/firewerks.png'
 import lazardala from '../Images/lazardala.png'
 import motherMycelia from '../Images/mother mycelia.png'
-import { FullScreen, useFullScreenHandle } from "react-full-screen";
-
 
 const Portfolio = () => {
-    const handle = useFullScreenHandle()
-
 
   return (
     <>
-      <div className="portfolioPg">
-        <Container className="projectCardsContainer">
+    <div className="portfolioPg">
+      <Container className="projectCardsContainer" >
 
+        <div className="projectLinks">          
+          <img
+            className="projectImg"
+            src={birdRainbow}
+            alt="bird rainbow"
+          />
+        </div>
 
-          <div className="projectLinks" >
-           <FullScreen handle={handle}>
-            Any fullscreen content here
-           </FullScreen>
-            <img
-            onClick={handle.enter}
+        <div className="projectLinks">
+          <img
+            className="projectImg"
+            src={bonkerz2}
+            alt="bonkerz 2"
+          />
+        </div>
+
+        <div className="projectLinks">
+          <img
+            className="projectImg"
+            src={firewerks}
+            alt="fire werks"
+          />
+        </div>
+
+        <div className="projectLinks">
+          <img
+            className="projectImg"
+            src={lazardala}
+            alt="lazar dala"
+          />
+        </div>
+
+        <div className="projectLinks">
+          <img 
               className="projectImg"
-              src={birdRainbow}
-              alt="bird rainbow"
-            />
-          </div>
+              src={motherMycelia} 
+              alt="mother mycelia" 
+          />
+        </div>
 
-
-
-          <div className="projectLinks" >
-            <img
-              className="projectImg"
-              src={bonkerz2}
-              alt="bonkerz 2"
-            />
-          </div>
-
-          <div className="projectLinks" >
-            <img
-              className="projectImg"
-              src={firewerks}
-              alt="fire werks"
-            />
-          </div>
-
-          <div className="projectLinks" >
-            <img
-              className="projectImg"
-              src={lazardala}
-              alt="lazar dala"
-            />
-          </div>
-
-          <div className="projectLinks" >
-            <img 
-                className="projectImg"
-                src={motherMycelia} 
-                alt="mother mycelia" 
-            />
-          </div>
-
-        </Container>
-      </div>
-    </>
+      </Container>
+    </div>
+  </>
   );
 };
 
