df740ca3745e9183e3b3bc7fda62ca4be0084885
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-11-30 09:42:47 -0600

    format documents with prettier

--- src/components/PlusData.jsx
+++ src/components/PlusData.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import * as ImIcons from 'react-icons/im'
-import * as VscIcons from 'react-icons/vsc'
-import * as BsIcons from 'react-icons/bs'
-
+import React from "react";
+import * as ImIcons from "react-icons/im";
+import * as VscIcons from "react-icons/vsc";
+import * as BsIcons from "react-icons/bs";
 
 export const plusData = [
-    {
-        title: "Facebook",
-        url: "https://www.facebook.com/bytordesign",
-        icon: <ImIcons.ImFacebook />,
-        cName: "plus-text",
-    },
-    {
-        title: "Instagram",
-        url: "https://www.instagram.com/xoxbytorxox/?fbclid=IwAR19MgrBbb4N8hvQPD_FDv5I2ozZsneg21j4e9zJ0Y_I1ORWFM1C-_Yz804",
-        icon: <BsIcons.BsInstagram/>,
-        cName: "plus-text",
-    },
-    {
-        title: "GitHub",
-        url: "https://github.com/NicBab/Bytor_Designs",
-        icon: <VscIcons.VscGithub />,
-        cName: "plus-text",
-    },
-]
+  {
+    title: "Facebook",
+    url: "https://www.facebook.com/bytordesign",
+    icon: <ImIcons.ImFacebook />,
+    cName: "plus-text",
+  },
+  {
+    title: "Instagram",
+    url: "https://www.instagram.com/xoxbytorxox/?fbclid=IwAR19MgrBbb4N8hvQPD_FDv5I2ozZsneg21j4e9zJ0Y_I1ORWFM1C-_Yz804",
+    icon: <BsIcons.BsInstagram />,
+    cName: "plus-text",
+  },
+  {
+    title: "GitHub",
+    url: "https://github.com/NicBab/Bytor_Designs",
+    icon: <VscIcons.VscGithub />,
+    cName: "plus-text",
+  },
+];
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,64 +1,59 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-import React from 'react'
+import React from "react";
 import "./css/Portfolio.css";
 import { Container } from "react-bootstrap";
-import birdRainbow from '../Images/BIRDRNBOW.png'
-import bonkerz2 from '../Images/bonkerz2.png'
-import firewerks from '../Images/firewerks.png'
-import lazardala from '../Images/lazardala.png'
-import motherMycelia from '../Images/mother mycelia.png'
+import birdRainbow from "../Images/BIRDRNBOW.png";
+import bonkerz2 from "../Images/bonkerz2.png";
+import firewerks from "../Images/firewerks.png";
+import lazardala from "../Images/lazardala.png";
+import motherMycelia from "../Images/mother mycelia.png";
 
 const Portfolio = () => {
-
   return (
     <>
-    <div className="portfolioPg">
-      <Container className="projectCardsContainer" >
+      <div className="portfolioPg">
+        <Container className="projectCardsContainer">
+          <div className="projectLinks">
+            <img 
+              className="projectImg" 
+              src={birdRainbow} 
+              alt="bird rainbow" 
+            />
+          </div>
 
-        <div className="projectLinks">          
-          <img
-            className="projectImg"
-            src={birdRainbow}
-            alt="bird rainbow"
-          />
-        </div>
+          <div className="projectLinks">
+            <img 
+              className="projectImg" 
+              src={bonkerz2} 
+              alt="bonkerz 2" 
+            />
+          </div>
 
-        <div className="projectLinks">
-          <img
-            className="projectImg"
-            src={bonkerz2}
-            alt="bonkerz 2"
-          />
-        </div>
+          <div className="projectLinks">
+            <img 
+              className="projectImg" 
+              src={firewerks} 
+              alt="fire werks" 
+            />
+          </div>
 
-        <div className="projectLinks">
-          <img
-            className="projectImg"
-            src={firewerks}
-            alt="fire werks"
-          />
-        </div>
+          <div className="projectLinks">
+            <img 
+              className="projectImg" 
+              src={lazardala} 
+              alt="lazar dala" 
+            />
+          </div>
 
-        <div className="projectLinks">
-          <img
-            className="projectImg"
-            src={lazardala}
-            alt="lazar dala"
-          />
-        </div>
-
-        <div className="projectLinks">
-          <img 
+          <div className="projectLinks">
+            <img
               className="projectImg"
-              src={motherMycelia} 
-              alt="mother mycelia" 
-          />
-        </div>
-
-      </Container>
-    </div>
-  </>
+              src={motherMycelia}
+              alt="mother mycelia"
+            />
+          </div>
+        </Container>
+      </div>
+    </>
   );
 };
 
