f7d36ebfcdb405f7c874c92ecb83530f872d12e9
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-07-11 21:14:54 -0500

    added galler.map data.js to portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,111 +1,18 @@
 import React from "react";
 import "./css/Portfolio.css";
 import { Container } from "react-bootstrap";
-import birdRainbow from "../Images/BIRDRNBOW.png";
-import bonkerz2 from "../Images/bonkerz2.png";
-import firewerks from "../Images/firewerks.png";
-import lazardala from "../Images/lazardala.png";
-import motherMycelia from "../Images/mother mycelia.png";
-import birdbones from "../Images/birdbones.jpg"
-import crclspstr_ps from "../Images/crclspstr_ps.jpg"
-import flier from "../Images/flier.jpg"
-import newVectorCharacter from "../Images/new_vector_character.png"
-import headsplosion_01 from "../Images/headsplosion_01.jpg"
-import newCollageScience from "../Images/newcollagescience.jpg"
+import { gallery } from "../data";
 
 const Portfolio = () => {
   return (
     <>
       <div className="portfolioPg">
         <Container className="projectCardsContainer">
-
-        <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={crclspstr_ps} 
-              alt="crclspstr ps" 
-            />
-          </div>
-
-        <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={flier} 
-              alt="flier" 
-            />
-          </div>
-
-        <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={newVectorCharacter} 
-              alt="new Vector Character" 
-            />
-          </div>
-
-          <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={headsplosion_01} 
-              alt="head splosion" 
-            />
-          </div>
-
-          <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={newCollageScience} 
-              alt="new collage science" 
-            />
-          </div>
-
-        <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={birdbones} 
-              alt="bird bones" 
-            />
-          </div>
-
-          <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={birdRainbow} 
-              alt="bird rainbow" 
-            />
-          </div>
-
-          <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={bonkerz2} 
-              alt="bonkerz 2" 
-            />
-          </div>
-
-          <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={firewerks} 
-              alt="fire werks" 
-            />
-          </div>
-
-          <div className="projectLinks">
-            <img 
-              className="projectImg" 
-              src={lazardala} 
-              alt="lazar dala" 
-            />
-          </div>
-
-          <div className="projectLinks">
-            <img
-              className="projectImg"
-              src={motherMycelia}
-              alt="mother mycelia"
-            />
-          </div>
+          {gallery.map((item) => (
+            <div className="projectLinks">
+              <img className="projectImg" src={item.img} alt="" />
+            </div>
+          ))}
         </Container>
       </div>
     </>
