b83d6fda5f9bda485578cc3f72d3ab927d65252f
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-08-11 17:27:15 -0500

    delete unused code

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -72,34 +72,6 @@ const Image = styled.img`
 
 `;
 
-const InfoContainer = styled.div`
-  flex: 1;
-  padding: 50px;
-`;
-
-const Title = styled.h1`
-  font-size: 70px;
-`;
-
-const Desc = styled.p`
-  margin: 50px 0px;
-  font-size: 20px;
-  font-weight: 500;
-  letter-spacing: 3px;
-`;
-
-const Button = styled.button`
-  padding: 10px;
-  font-size: 20px;
-  border-radius: 10px;
-  background-color: ;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #88d8c0;
-    
-  }
-`;
 
 const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
